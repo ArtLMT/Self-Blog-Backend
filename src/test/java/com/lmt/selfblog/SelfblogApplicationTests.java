308f840232a48edcfb90ccc687935295cd3450bd
Lê Minh Thành <leminhthanh280425@gmail.com>
2026-05-09 14:56:00 +0700

    initialize commit

---
+++ src/test/java/com/lmt/selfblog/SelfblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmt.selfblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
